73d082c5b69fab12cf077346290e94b69d511055
anhnh-3008 <nguyenhoanganh01a2@gmail.com>
2022-12-24 19:47:39 +0700

    Lession05 - List rendering

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            users: [
+              { id: 1, name: "user A" },
+              { id: 2, name: "user B" },
+              { id: 3, name: "user C" }
+            ],
+            sizes: ['Size S', 'Size L', 'Size XL', 'Size XXL']
         }
     }
 })
