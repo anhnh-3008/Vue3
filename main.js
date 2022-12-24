2cce821477f4c71fd24c5d41e2c9fb5062784743
anhnh-3008 <nguyenhoanganh01a2@gmail.com>
2022-12-24 22:04:49 +0700

    Lession07 - Class & Style binding

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
             inStock: false,
+            inventories: 10,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -15,7 +16,18 @@ const app = Vue.createApp({
     },
     methods: {
         addToCart() {
+          if (this.inventories > 0) {
             this.cart += 1
+            this.inventories -=1
+            if (this.inventories == 0) this.inStock = true
+          }
+        },
+        removeToCart() {
+          if (this.cart > 0) {
+            this.cart -= 1
+            this.inventories +=1
+            this.inStock = false
+          }
         },
         updateImage(variantImage) {
             this.image = variantImage
